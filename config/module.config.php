f81c5e4b08a152b5edfa80bdeb8b14e8486cd6f7
Skpd <dm.skpd@gmail.com>
2014-01-10 21:51:10 +0400

    added options, close #5

--- config/module.config.php
+++ config/module.config.php
@@ -6,6 +6,15 @@ return [
     'service_manager' => [
         'invokables' => [
             'Skpd\ProfilerToolbar\ProfilerCollector' => 'Skpd\ProfilerToolbar\Collector\XhprofCollector'
+        ],
+        'factories' => [
+            'Skpd\ProfilerToolbar\Options' => function ($serviceManager) {
+                /** @var \Zend\ServiceManager\ServiceManager $serviceManager */
+                $config = $serviceManager->get('Configuration');
+                $config = isset($config['profiler-toolbar']) ? $config['profiler-toolbar'] : [];
+
+                return new Options($config);
+            }
         ]
     ],
     'view_manager' => [
---
+++ src/Options.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Skpd\ProfilerToolbar;
+
+use Zend\Stdlib\AbstractOptions;
+
+class Options extends AbstractOptions
+{
+
+}
